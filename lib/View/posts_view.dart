8168148374792797f433ab237ecce69a82686405
Patrik Cesnek <pcesnek290@gmail.com>
2024-02-27 11:06:31 +0100

    Change postsView to stateful widget

--- lib/View/posts_view.dart
+++ lib/View/posts_view.dart
@@ -2,31 +2,53 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm_flutter/ViewModel/posts_view_model.dart';
 
-class PostView extends StatelessWidget {
+class PostView extends StatefulWidget {
   final PostsViewModel viewModel;
 
   const PostView({required this.viewModel});
 
   @override
+  State<PostView> createState() => _PostViewState();
+}
+
+class _PostViewState extends State<PostView> {
+
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.fetchPosts();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Posts'),
-      ),
-      body: ListView.builder(
-        itemCount: viewModel.posts.length,
-        itemBuilder: (context, index) {
-          final post = viewModel.posts[index];
-          return ListTile(
-            title: Text(post.title),
-            subtitle: Text(post.body),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => viewModel.fetchPosts(),
-        child: Icon(Icons.refresh),
-      ),
-    );
+    if (widget.viewModel.isLoading) {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Posts ${widget.viewModel.posts.length}'),
+        ),
+        body: ListView.builder(
+          itemCount: widget.viewModel.posts.length,
+          itemBuilder: (context, index) {
+            final post = widget.viewModel.posts[index];
+            return Card(
+              child: ListTile(
+                title: Text(post.title),
+                subtitle: Text(post.body),
+              ),
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => widget.viewModel.fetchPosts(),
+          child: Icon(Icons.refresh),
+        ),
+      );
+    }
   }
 }
--- lib/ViewModel/posts_view_model.dart
+++ lib/ViewModel/posts_view_model.dart
@@ -3,14 +3,21 @@ import '../Model/posts.dart';
 import 'package:http/http.dart' as http;
 
 class PostsViewModel {
+  bool isLoading = false;
   List<Posts> _posts = [];
 
   List<Posts> get posts => _posts;
 
   Future<void> fetchPosts() async {
+    isLoading = true;
+    print("Should start loading... result is: ${isLoading}");
     List<Posts> fetchedPosts = await _fetchPostsFromApi();
 
     _posts = fetchedPosts;
+    print(fetchedPosts.first);
+    print(fetchedPosts.length);
+    isLoading = false;
+    print("Should stop loading... result is: ${isLoading}");
   }
 
   Future<List<Posts>> _fetchPostsFromApi() async {
@@ -20,8 +27,10 @@ class PostsViewModel {
       final List<dynamic> data = json.decode(response.body);
       //TODO: Delete the print statement!!
       print(data);
+
       return data.map((json) => Posts(userId: json['userId'], id: json['id'], title: json['title'], body: json['body'])).toList();
     } else {
+      print("There was an error fetching data");
       throw Exception('Failed to load posts');
     }
   }
